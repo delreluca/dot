bcf14b1ec6f27ba03ae23612e36af15fbbe0cc90
Luca Del Re <v.commit@ldr.name>
2025-05-26 13:52:45 +0200

    Improve path setup in fish config
    Add .local/bin
    Opt out of .NET telemetry and remove error message when not installed

--- .config/fish/conf.d/ldr.fish
+++ .config/fish/conf.d/ldr.fish
@@ -1,14 +1,18 @@
 set -gx EDITOR vim
 
+# ~/.local/bin
+if test -d ~/.local/bin
+	fish_add_path -g ~/.local/bin
+end
+
 # MacPorts
 if test -x /opt/local/bin/port
 	fish_add_path -g /opt/local/bin /opt/local/sbin
 end
 
 # dotnet (installation via script)
-if test -d ~/.dotnet
+if test -x ~/.dotnet/dotnet
 	fish_add_path -g ~/.dotnet
-end
-if which dotnet
+	set -gx DOTNET_CLI_TELEMETRY_OPTOUT 1
 	set -gx DOTNET_ROOT (dirname (which dotnet))
 end
