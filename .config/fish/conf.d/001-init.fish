0e2d8187dec2c052986fa7c998a62df9b0fb7d3d
Luca Del Re <v.commit@ldr.name>
2026-07-13 13:52:15 +0200

    Use vim as man pager

--- .config/fish/conf.d/001-init.fish
+++ .config/fish/conf.d/001-init.fish
@@ -1,8 +1,15 @@
 set -gx EDITOR vim
 
+if test (readlink (command -v man) 2>/dev/null; or echo '') = 'mandoc'
+    # mandoc will write a temporary file instead
+    set -gx MANPAGER "vim +MANPAGER --not-a-term"
+else
+    set -gx MANPAGER "vim +MANPAGER --not-a-term -"
+end
+
 # honoured by: gh
 set -gx DO_NOT_TRACK 1
 
 if test -d ~/.local/bin
-	fish_add_path -g ~/.local/bin
+    fish_add_path -g ~/.local/bin
 end
